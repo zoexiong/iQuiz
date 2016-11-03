cdc025db034f353e1b856e2a3f8374dc3370ff4e
zoexiong <xiongsy@uw.edu>
2016-11-03 12:51:16 -0700

    add subjects and values

---
+++ iQuiz/subject.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  iQuiz
+//
+//  Created by Just on 16/11/3.
+//  Copyright © 2016年 Just. All rights reserved.
+//
+
+import UIKit
+
+class Subject {
+    var subjectTitle : String
+    var subjectDescription : String
+    var subjectIcon : UIImage?
+    
+    init(_ subjectTitle:String, _ subjectDescription:String, _ subjectIcon:UIImage?){
+        self.subjectTitle = subjectTitle
+        self.subjectIcon = subjectIcon
+        self.subjectDescription = subjectDescription
+    }
+}
